7faf8c021b3bb4956dba8ec5080232b6eaaf60cb
Ran X <aldousxin1@gmail.com>
2025-02-05 16:08:48 -0800

    first commit

---
+++ dist/global.js
@@ -0,0 +1,1 @@
+!function(o,e){"object"==typeof exports&&"object"==typeof module?module.exports=e():"function"==typeof define&&define.amd?define("global",[],e):"object"==typeof exports?exports.global=e():o.global=e()}(this,(()=>(console.log("Hello, world!aoeu"),{})));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    ["global"]: "./src/index.js",
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    library: "[name]",
+    libraryTarget: "umd",
+    globalObject: "this",
+    umdNamedDefine: true,
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+};
